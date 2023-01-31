00b7c4f8a890cf09827aacf7a4063577115c949e
sebahag <101423619+sebahag@users.noreply.github.com>
2023-01-31 10:49:05 +0200

    tested the script and it worked

--- downloadvideo.py
+++ downloadvideo.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 #Downloads the youtube-video in the highest possible resolution in the directory of the script
-url = r"url-path of the video"
+url = r""
 the_video = YouTube(url)
 the_video = the_video.streams.get_highest_resolution()
 the_video.download()
